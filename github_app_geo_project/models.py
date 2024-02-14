b8ba5fc9bca13cc4dd69f9190822591a93f81257
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-02-14 18:30:55 +0100

    Add output status, enum

--- github_app_geo_project/models.py
+++ github_app_geo_project/models.py
@@ -1,5 +1,6 @@
 """Models for the GitHub App Geo Project."""
 
+import enum
 import logging
 from datetime import datetime
 from typing import Any
@@ -8,7 +9,7 @@ import sqlalchemy
 import sqlalchemy.ext.declarative
 import sqlalchemy.orm
 import sqlalchemy.sql.functions
-from sqlalchemy import JSON, DateTime, Integer, Unicode
+from sqlalchemy import JSON, DateTime, Enum, Integer, Unicode
 from sqlalchemy.orm import Mapped, mapped_column
 
 _LOGGER = logging.getLogger(__name__)
@@ -16,19 +17,19 @@ _LOGGER = logging.getLogger(__name__)
 DBSession = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker())
 # Base = sqlalchemy.ext.declarative.declarative_base()
 
-# - new: the job is added in the queue
-# - pending: the job is processing
-# - error: the job is in error
-# - done: the job is finished with success
-STATUS_NEW = "new"
-STATUS_PENDING = "pending"
-STATUS_ERROR = "error"
-STATUS_DONE = "done"
-
 _SCHEMA = "github_app"
 
 
 Base = sqlalchemy.orm.declarative_base()
+
+
+class JobStatus(enum.Enum):
+    """Enum for the status of the job."""
+
+    new = "new"
+    pending = "pending"
+    error = "error"
+    done = "done"
 
 
 class Queue(Base):  # type: ignore[misc,valid-type]
@@ -38,7 +39,9 @@ class Queue(Base):  # type: ignore[misc,valid-type]
     __table_args__ = {"schema": _SCHEMA}
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    status: Mapped[str] = mapped_column(Unicode, nullable=False, default=STATUS_NEW, index=True)
+    status: Mapped[JobStatus] = mapped_column(
+        JobStatus, native_enum=False, nullable=False, default=JobStatus.new, index=True
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=sqlalchemy.sql.functions.now(), index=True  # type: ignore[no-untyped-call]
     )
@@ -52,6 +55,22 @@ class Queue(Base):  # type: ignore[misc,valid-type]
         return f"Queue {self.id} [{self.status}]"
 
 
+class OutputStatus(enum.Enum):
+    """Enum for the status of the output."""
+
+    error = "error"
+    success = "success"
+
+
+class AccessType(enum.Enum):
+    """Enum for the access type of the output."""
+
+    public = "public"
+    pull = "pull"
+    push = "push"
+    admin = "admin"
+
+
 class Output(Base):  # type: ignore[misc,valid-type]
     """SQLAlchemy model for the output entries."""
 
@@ -59,7 +78,8 @@ class Output(Base):  # type: ignore[misc,valid-type]
     __table_args__ = {"schema": _SCHEMA}
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    repository: Mapped[str] = mapped_column(Unicode, nullable=False)
-    access_type: Mapped[str] = mapped_column(Unicode, nullable=False)
+    status: Mapped[OutputStatus] = mapped_column(OutputStatus, native_enum=False, nullable=False, index=True)
+    repository: Mapped[str] = mapped_column(Unicode, nullable=False, index=True)
+    access_type: Mapped[AccessType] = mapped_column(AccessType, native_enum=False, nullable=False)
     title: Mapped[str] = mapped_column(Unicode, nullable=False)
     data: Mapped[str] = mapped_column(Unicode, nullable=False)
