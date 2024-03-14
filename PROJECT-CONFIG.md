c41fe9a3d9426612d1d7b4521e07f79e6f024aa1
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-03-14 16:40:19 +0100

    Fix the project schema

--- PROJECT-CONFIG.md
+++ PROJECT-CONFIG.md
@@ -1,7 +1,18 @@
 # GitHub application project configuration
 
+## Additional Properties
+
+- **Additional Properties** _(object)_
+  - **All of**
+    - : Refer to _[#/$defs/module-configuration](#%24defs/module-configuration)_.
+    - : Refer to _[#/$defs/project-configuration](#%24defs/project-configuration)_.
+
+## Properties
+
+- **`profile`** _(string)_: The profile to use for the project.
+
 ## Definitions
 
 - <a id="%24defs/project-configuration"></a>**`project-configuration`** _(object)_
-- <a id="%24defs/module_configuration"></a>**`module_configuration`** _(object)_
-  - **`enabled`** _(boolean)_: Enable the module.
+- <a id="%24defs/module-configuration"></a>**`module-configuration`** _(object)_
+  - **`enabled`** _(boolean)_: Enable the module. Default: `true`.
--- github_app_geo_project/project_configuration.py
+++ github_app_geo_project/project_configuration.py
@@ -3,15 +3,39 @@ Automatically generated file from a JSON schema.
 """
 
 
-from typing import TypedDict
+from typing import TypedDict, Union
+
+GithubApplicationProjectConfiguration = Union[
+    dict[str, "ModuleConfiguration"], "GithubApplicationProjectConfigurationTyped"
+]
+"""
+GitHub application project configuration.
 
 
-class GithubApplicationProjectConfiguration(TypedDict, total=False):
-    """GitHub application project configuration."""
+WARNING: Normally the types should be a mix of each other instead of Union.
+See: https://github.com/camptocamp/jsonschema-gentypes/issues/7
+"""
 
+
+class GithubApplicationProjectConfigurationTyped(TypedDict, total=False):
     profile: str
     """
     Profile.
 
     The profile to use for the project
     """
+
+
+class ModuleConfiguration(TypedDict, total=False):
+    """Module configuration."""
+
+    enabled: bool
+    """
+    Enable the module
+
+    default: True
+    """
+
+
+_MODULE_CONFIGURATION_ENABLED_DEFAULT = True
+""" Default value of the field path 'Module configuration enabled' """
