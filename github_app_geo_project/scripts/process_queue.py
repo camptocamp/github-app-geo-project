15e218e9d86f435757ebbdd9b1b2d608c21325f4
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-03-22 08:34:07 +0100

    Don't creates tow times the same objects

--- github_app_geo_project/scripts/process_queue.py
+++ github_app_geo_project/scripts/process_queue.py
@@ -124,11 +124,11 @@ def main() -> None:
                     _LOGGER.error("Unknown module %s", job_module)
                     continue
 
-                github_app = configuration.get_github_application(config, job_application, owner, repository)
+                github_objects = configuration.get_github_objects(config, job_application)
+                github_app = configuration.get_github_application(config, github_objects, owner, repository)
 
                 issue_data = ""
                 if current_module.required_issue_dashboard():
-                    github_objects = configuration.get_github_objects(config, job_application)
                     repository_full = f"{owner}/{repository}"
                     repo = github_app.application.get_repo(repository_full)
                     open_issues = repo.get_issues(
