d8258701a864b190732d368808db3046e0a64029
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-04-04 11:48:46 +0200

    Add audit test

---
+++ tests/audit_test.py
@@ -0,0 +1,31 @@
+from github_app_geo_project.module.audit import _format_issue_data, _parse_issue_data
+
+
+def test_parse_issue_data() -> None:
+    issue_data = "\n".join(
+        [
+            "### Key1",
+            "Value1",
+            "Value2",
+            "### Key2",
+            "Value3",
+            "",
+        ]
+    )
+    expected_result = {"Key1": ["Value1", "Value2"], "Key2": ["Value3"]}
+    assert _parse_issue_data(issue_data) == expected_result
+
+
+def test_format_issue_data() -> None:
+    issue_data = {"Key1": ["Value1", "Value2"], "Key2": ["Value3"]}
+    expected_result = "\n".join(
+        [
+            "### Key1",
+            "Value1",
+            "Value2",
+            "### Key2",
+            "Value3",
+            "",
+        ]
+    )
+    assert _format_issue_data(issue_data) == expected_result
