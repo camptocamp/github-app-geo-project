376df66edee55f189c9a817dc0645e6b1602bf89
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-04-10 13:24:19 +0200

    Fix lost empty lines

--- tests/audit_test.py
+++ tests/audit_test.py
@@ -18,6 +18,25 @@ def test_parse_issue_data() -> None:
     assert _parse_issue_data(issue_data) == expected_result
 
 
+def test_parse_issue_data_space() -> None:
+    issue_data = "\n".join(
+        [
+            "",
+            "### Key1",
+            "",
+            "Value1",
+            "",
+            "Value2",
+            "",
+            "### Key2",
+            "Value3",
+            "",
+        ]
+    )
+    expected_result = {"Key1": ["Value1", "", "Value2"], "Key2": ["Value3"]}
+    assert _parse_issue_data(issue_data) == expected_result
+
+
 def test_format_issue_data() -> None:
     issue_data = {"Key1": ["Value1", "Value2"], "Key2": ["Value3"]}
     expected_result = "\n".join(
