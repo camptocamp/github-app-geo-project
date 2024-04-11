a92cefd79044528dd9084a4f4f262bf1ef8fb89e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-04-11 10:01:50 +0200

    Don't use Docker on dpkg

--- AUDIT-CONFIG.md
+++ AUDIT-CONFIG.md
@@ -3,15 +3,26 @@
 ## Properties
 
 - **`audit`** _(object)_: Cannot contain additional properties.
-  - **`files-no-install`** _(array)_: Dependency files that will not be installed. Default: `[]`.
-    - **Items** _(string)_
-  - **`pip-install-arguments`** _(array)_: Arguments to pass to pip install. Default: `["--user"]`.
-    - **Items** _(string)_
-  - **`pipenv-sync-arguments`** _(array)_: Arguments to pass to pipenv sync. Default: `[]`.
-    - **Items** _(string)_
-  - **`monitor-arguments`** _(array)_: Arguments to pass to snyk monitor. Default: `["--all-projects"]`.
-    - **Items** _(string)_
-  - **`test-arguments`** _(array)_: Arguments to pass to snyk test. Default: `["--all-projects", "--fail-on=upgradable", "--severity-threshold=medium"]`.
-    - **Items** _(string)_
-  - **`fix-arguments`** _(array)_: Arguments to pass to snyk fix. Default: `["--all-projects"]`.
-    - **Items** _(string)_
+  - **`snyk`** _(object)_: Cannot contain additional properties.
+    - **`enabled`** _(boolean)_: Enable Snyk audit. Default: `true`.
+    - **`files-no-install`** _(array)_: Dependency files that will not be installed. Default: `[]`.
+      - **Items** _(string)_
+    - **`pip-install-arguments`** _(array)_: Arguments to pass to pip install. Default: `["--user"]`.
+      - **Items** _(string)_
+    - **`pipenv-sync-arguments`** _(array)_: Arguments to pass to pipenv sync. Default: `[]`.
+      - **Items** _(string)_
+    - **`monitor-arguments`** _(array)_: Arguments to pass to snyk monitor. Default: `["--all-projects"]`.
+      - **Items** _(string)_
+    - **`test-arguments`** _(array)_: Arguments to pass to snyk test. Default: `["--all-projects", "--fail-on=upgradable", "--severity-threshold=medium"]`.
+      - **Items** _(string)_
+    - **`fix-arguments`** _(array)_: Arguments to pass to snyk fix. Default: `["--all-projects"]`.
+      - **Items** _(string)_
+  - **`dpkg`** _(object)_: Cannot contain additional properties.
+    - **`enabled`** _(boolean)_: Enable Dpkg audit. Default: `true`.
+    - **`sources`** _(object)_: Can contain additional properties. Default: `{"ubuntu_22_04": [{"url": "http://archive.ubuntu.com/ubuntu", "distribution": "jammy", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "jammy-security", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "jammy-updates", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "jammy-backports", "components": ["main", "restricted", "universe", "multiverse"]}], "ubuntu_24_04": [{"url": "http://archive.ubuntu.com/ubuntu", "distribution": "noble", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "noble-security", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "noble-updates", "components": ["main", "restricted", "universe", "multiverse"]}, {"url": "http://security.ubuntu.com/ubuntu", "distribution": "noble-backports", "components": ["main", "restricted", "universe", "multiverse"]}], "debian_11": [{"url": "http://deb.debian.org/debian", "distribution": "bullseye", "components": ["main", "contrib", "non-free"]}, {"url": "http://deb.debian.org/debian", "distribution": "bullseye-updates", "components": ["main", "contrib", "non-free"]}, {"url": "http://security.debian.org/debian-security", "distribution": "bullseye-security", "components": ["main", "contrib", "non-free"]}], "debian_12": [{"url": "http://deb.debian.org/debian", "distribution": "bookworm", "components": ["main", "contrib", "non-free"]}, {"url": "http://deb.debian.org/debian", "distribution": "bookworm-updates", "components": ["main", "contrib", "non-free"]}, {"url": "http://security.debian.org/debian-security", "distribution": "bookworm-security", "components": ["main", "contrib", "non-free"]}]}`.
+      - **Additional properties** _(array)_
+        - **Items** _(object)_
+          - **`url`** _(string)_: URL of the source.
+          - **`distribution`** _(string)_: Distribution of the source.
+          - **`components`** _(array)_: Components of the source.
+            - **Items** _(string)_
--- github_app_geo_project/module/audit/utils.py
+++ github_app_geo_project/module/audit/utils.py
@@ -6,10 +6,11 @@ import datetime
 import json
 import logging
 import os.path
-import re
 import subprocess  # nosec
 
+import apt_repo
 import c2cciutils.security
+import debian_inspector.version
 import pygments.formatters
 import pygments.lexers
 import yaml  # nosec
@@ -21,7 +22,7 @@ _LOGGING = logging.getLogger(__name__)
 
 
 def snyk(
-    branch: str, config: configuration.AuditConfiguration, local_config: configuration.AuditConfiguration
+    branch: str, config: configuration.SnykConfiguration, local_config: configuration.SnykConfiguration
 ) -> tuple[list[utils.Message], utils.Message]:
     """
     Audit the code with Snyk.
@@ -205,103 +206,69 @@ def outdated_versions(
     return errors
 
 
-def dpkg() -> list[utils.Message]:
+_SOURCES = {}
+
+
+def _get_sources(
+    dist: str, config: configuration.DpkgConfiguration, local_config: configuration.DpkgConfiguration
+) -> apt_repo.APTSources:
+    """
+    Get the sources for the distribution.
+    """
+    if dist not in _SOURCES:
+        conf = local_config.get("sources", config.get("sources", configuration.DPKG_SOURCES_DEFAULT))
+        if dist not in conf:
+            raise ValueError(f"The distribution {dist} is not in the configuration")
+        _SOURCES[dist] = apt_repo.APTSources(
+            [
+                apt_repo.APTRepository(
+                    source["url"],
+                    source["distribution"],
+                    source["components"],
+                )
+                for source in conf[dist]
+            ]
+        )
+
+    return _SOURCES[dist]
+
+
+_PACKAGE_VERSION: dict[str, str] = {}
+
+
+def _get_packages_version(
+    package: str, config: configuration.DpkgConfiguration, local_config: configuration.DpkgConfiguration
+) -> str | None:
+    """Get the version of the package."""
+    if package not in _PACKAGE_VERSION:
+        dist, pkg = package.split("/")
+        sources = _get_sources(dist, config, local_config)
+        versions = sorted(
+            [
+                debian_inspector.version.Version.from_string(package.version)
+                for package in sources.get_packages_by_name(pkg)
+            ]
+        )
+        if not versions:
+            _LOGGING.warning("No version found for %s", package)
+            return None
+        else:
+            _PACKAGE_VERSION[package] = versions[-1].to_string()
+    return _PACKAGE_VERSION[package]
+
+
+def dpkg(config: configuration.DpkgConfiguration, local_config: configuration.DpkgConfiguration) -> None:
     """Update the version of packages in the file ci/dpkg-versions.yaml."""
     if not os.path.exists("ci/dpkg-versions.yaml"):
-        return [utils.HtmlMessage("The file ci/dpkg-versions.yaml does not exist")]
+        _LOGGING.error("The file ci/dpkg-versions.yaml does not exist")
 
-    cache: dict[str, dict[str, str]] = {}
-    results: list[utils.Message] = []
     with open("ci/dpkg-versions.yaml", encoding="utf-8") as versions_file:
         versions_config = yaml.load(versions_file, Loader=yaml.SafeLoader)
         for versions in versions_config.values():
             for package_full in versions.keys():
-                dist, package = package_full.split("/")
-                if dist not in cache:
-                    correspondence = {
-                        "ubuntu_22_04": ("ubuntu", "22.04"),
-                        "debian_11": ("debian", "11"),
-                        "debian_12": ("debian", "12"),
-                    }
-                    if dist in correspondence:
-                        images, tag = correspondence[dist]
-                        proc = subprocess.run(  # nosec # pylint: disable=subprocess-run-check
-                            ["docker", "rm", "--force", "apt"],
-                            capture_output=True,
-                            encoding="utf-8",
-                        )
-                        if proc.returncode == 0:
-                            message = utils.ansi_proc_message(proc)
-                            message.title = "Error while removing the container"
-                            _LOGGING.warning(message.to_html())
-                            results.append(message)
-                        proc = subprocess.run(  # nosec # pylint: disable=subprocess-run-check
-                            [
-                                "docker",
-                                "run",
-                                "--tty",
-                                "--interactive",
-                                "--detach",
-                                "--name=apt",
-                                "--entrypoint=",
-                                f"{images}:{tag}",
-                                "tail",
-                                "--follow",
-                                "/dev/null",
-                            ],
-                            capture_output=True,
-                            encoding="utf-8",
-                        )
-                        if proc.returncode != 0:
-                            message = utils.ansi_proc_message(proc)
-                            message.title = "Error while creating the container"
-                            _LOGGING.warning(message.to_html())
-                            results.append(message)
-
-                        proc = subprocess.run(  # nosec # pylint: disable=subprocess-run-check
-                            ["docker", "exec", "apt", "apt-get", "update"],
-                            capture_output=True,
-                            encoding="utf-8",
-                        )
-                        if proc.returncode != 0:
-                            message = utils.ansi_proc_message(proc)
-                            message.title = "Error while updating the container"
-                            _LOGGING.warning(message.to_html())
-                            results.append(message)
-
-                        package_re = re.compile(r"^([^ /]+)/[a-z-,]+ ([^ ]+) (all|amd64)( .*)?$")
-                        proc = subprocess.run(  # nosec # pylint: disable=subprocess-run-check
-                            ["docker", "exec", "apt", "apt", "list"],
-                            capture_output=True,
-                            encoding="utf-8",
-                        )
-                        if proc.returncode != 0:
-                            message = utils.ansi_proc_message(proc)
-                            message.title = "Error while listing the packages"
-                            _LOGGING.warning(message.to_html())
-                            results.append(message)
-                            return results
-                        for proc_line in proc.stdout.split("\n"):
-                            package_match = package_re.match(proc_line)
-                            if package_match is None:
-                                _LOGGING.debug("Not matching: %s", proc_line)
-                                continue
-                            cache.setdefault(dist, {})[package_match.group(1)] = package_match.group(2)
-
-                        proc = subprocess.run(  # nosec # pylint: disable=subprocess-run-check
-                            ["docker", "rm", "--force", "apt"],
-                            capture_output=True,
-                            encoding="utf-8",
-                        )
-                        if proc.returncode != 0:
-                            message = utils.ansi_proc_message(proc)
-                            message.title = "Error while removing the container"
-                            _LOGGING.warning(message.to_html())
-                            results.append(message)
-                if package in cache[dist]:
-                    versions[package_full] = cache[dist][package]
+                version = _get_packages_version(package_full, config, local_config)
+                if version:
+                    versions[package_full] = version
 
     with open("ci/dpkg-versions.yaml", "w", encoding="utf-8") as versions_file:
         yaml.dump(versions_config, versions_file, Dumper=yaml.SafeDumper)
-
-    return results
