a4ee13085f2a30fae1baca31513cdcc62942372b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-05-07 08:43:42 +0200

    Fix default pemissions

--- github_app_geo_project/views/home.py
+++ github_app_geo_project/views/home.py
@@ -56,7 +56,7 @@ def output(request: pyramid.request.Request) -> dict[str, Any]:
             # Impossible to remove this permission on GitHub, so we don't check it
             "metadata": "read",
             # Used to create and update the github checks
-            "checks": "write",
+            "actions": "write",
         }
         events = set()
 
