619b2d2d1cbe328533309db1efb0cc25e240f1e3
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-05-27 14:49:55 +0200

    Workflow: Test/fix success

---
+++ tests/test_module_workflow.py
@@ -0,0 +1,86 @@
+from unittest.mock import MagicMock
+
+import github
+import pytest
+
+from github_app_geo_project.module.workflow import Workflow
+
+
+@pytest.mark.asyncio
+async def test_process_success() -> None:
+    # Create a mock context
+    context = MagicMock()
+    context.transversal_status = {
+        "owner/repository": {
+            "workflow_name": {
+                "date": None,
+                "jobs": [],
+                "url": None,
+            },
+        }
+    }
+    context.github_project.owner = "owner"
+    context.github_project.repository = "repository"
+    repo = MagicMock()
+    context.github_project.github.get_repo.return_value = repo
+    repo.get_contents.side_effect = github.GithubException(status=404)
+    repo.default_branch = "master"
+    context.event_data = {
+        "workflow_run": {
+            "head_branch": "master",
+            "conclusion": "success",
+        },
+        "workflow": {
+            "name": "workflow_name",
+        },
+    }
+
+    # Create an instance of the Workflow class
+    workflow = Workflow()
+
+    # Call the process method
+    output = await workflow.process(context)
+
+    assert output.transversal_status == {}
+
+
+@pytest.mark.asyncio
+async def test_process_failure() -> None:
+    # Create a mock context
+    context = MagicMock()
+    context.transversal_status = {}
+    context.github_project.owner = "owner"
+    context.github_project.repository = "repository"
+    repo = MagicMock()
+    context.github_project.github.get_repo.return_value = repo
+    repo.get_contents.side_effect = github.GithubException(status=404)
+    repo.default_branch = "master"
+    context.event_data = {
+        "workflow_run": {
+            "head_branch": "master",
+            "conclusion": "failure",
+        },
+        "workflow": {
+            "name": "workflow_name",
+        },
+    }
+
+    # Create an instance of the Workflow class
+    workflow = Workflow()
+
+    # Call the process method
+    output = await workflow.process(context)
+
+    print(output)
+    # Assert the expected output
+    assert output.transversal_status == {
+        "owner/repository": {
+            "master": {
+                "workflow_name": {
+                    "date": None,
+                    "jobs": [],
+                    "url": None,
+                },
+            },
+        }
+    }
