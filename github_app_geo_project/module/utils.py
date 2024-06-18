8fb202ec174cc4e4e19b6c0490ac4e19e6fca51f
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-06-18 10:43:02 +0200

    Add debug message

--- github_app_geo_project/module/utils.py
+++ github_app_geo_project/module/utils.py
@@ -497,6 +497,7 @@ async def run_timeout(
     ------
     The standard output, the success, the logged message
     """
+    _LOGGER.debug("Run command: %s", shlex.join(command))
     async_proc = None
     try:
         async with asyncio.timeout(timeout):
