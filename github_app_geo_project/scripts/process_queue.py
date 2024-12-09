490a010f60945622b56cc37a6b490ee68e3183ed
Stéphane Brunner <stephane.brunner@camptocamp.com>
2024-12-09 10:44:06 +0100

    Fix if there is no text or no summary

--- github_app_geo_project/scripts/process_queue.py
+++ github_app_geo_project/scripts/process_queue.py
@@ -230,9 +230,9 @@ async def _process_job(
                 if result is not None and result.output:
                     check_output.update(result.output)
                 assert check_run is not None
-                if len(check_output["summary"]) > 65535:
+                if len(check_output.get("summary", "")) > 65535:
                     check_output["summary"] = check_output["summary"][:65532] + "..."
-                if len(check_output["text"]) > 65535:
+                if len(check_output.get("text", "")) > 65535:
                     check_output["text"] = check_output["text"][:65532] + "..."
                 try:
                     check_run.edit(
