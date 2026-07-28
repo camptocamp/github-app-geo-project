ed21bbddd5fbcf212af9c76e0080885ed4a503d0
Stéphane Brunner <stephane.brunner@camptocamp.com>
2026-07-28 06:23:56 +0000

    Fix output route template resolution for module templates

--- github_app_geo_project/views/dashboard.py
+++ github_app_geo_project/views/dashboard.py
@@ -3,43 +3,16 @@
 import logging
 from typing import Annotated, Any
 
-import anyio
 import sqlalchemy
 from fastapi import Depends, HTTPException, Request
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from github_app_geo_project import models, module
 from github_app_geo_project.module import modules
 from github_app_geo_project.security import User, get_user
-from github_app_geo_project.templates import (
-    markdown,
-    pprint_date,
-    pprint_duration,
-    pprint_full_date,
-    pprint_short_date,
-    sanitizer,
-)
+from github_app_geo_project.templates import render_template
 from github_app_geo_project.utils import HTML_FORMATTER
 
 _LOGGER = logging.getLogger(__name__)
-
-
-async def _render_template(renderer: str, data: dict[str, Any]) -> str:
-    package, path = renderer.split(":", 1)
-    package_dir = anyio.Path(__file__).parent.parent.parent / package
-    template_path = package_dir / path
-    env = Environment(
-        loader=FileSystemLoader(str(template_path.parent)),
-        autoescape=select_autoescape(),
-    )
-    env.filters["markdown"] = markdown
-    env.filters["sanitizer"] = sanitizer
-    env.filters["pprint_date"] = pprint_date
-    env.filters["pprint_short_date"] = pprint_short_date
-    env.filters["pprint_full_date"] = pprint_full_date
-    env.filters["pprint_duration"] = pprint_duration
-    template = env.get_template(template_path.name)
-    return template.render(data)
 
 
 async def dashboard(
@@ -81,7 +54,7 @@ async def dashboard(
         data.setdefault("styles", HTML_FORMATTER.get_style_defs())
 
         if output.renderer:
-            data["html"] = await _render_template(output.renderer, data)
+            data["html"] = await render_template(output.renderer, data)
 
     return {
         "request": request,
